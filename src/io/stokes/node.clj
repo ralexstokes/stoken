720fb8761fdd648f32877f664baf4130679acfcf
Alex Stokes <r.alex.stokes@gmail.com>
2018-01-14 13:33:46 -0800

    order imports

--- src/io/stokes/node.clj
+++ src/io/stokes/node.clj
@@ -1,13 +1,12 @@
 (ns io.stokes.node
   (:require
+   [com.stuartsierra.component :as component]
+   [io.stokes.state :as state]
+   [io.stokes.queue :as queue]
    [io.stokes.rpc :as rpc]
    [io.stokes.p2p :as p2p]
    [io.stokes.miner :as miner]
-   [io.stokes.scheduler :as scheduler]
-   [io.stokes.state :as state]
-   [io.stokes.queue :as queue]
-   [com.stuartsierra.component :as component]
-   [clojure.core.async :as async]))
+   [io.stokes.scheduler :as scheduler]))
 
 (defn from
   "constructs an instance of the system ready to run given the config"
